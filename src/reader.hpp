5d55dc09134a02466781e17e6969a6d0a9295df7
Nunzio <nunzioonorati@gmail.com>
2018-03-15 20:16:20 +0100

    out

---
+++ src/reader.hpp
@@ -0,0 +1,32 @@
+#ifndef reader_hpp
+#define reader_hpp
+
+#include "dependencies.hpp"
+
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+
+namespace reply {
+	
+	class Reader {
+	
+	public:
+		map<int,Provider*> getProviders();
+		map<int,Service*> getServices();
+		map<int,Country*> getCountries();
+		map<int,Project*> getProjects();
+		Reader(string);
+		
+  private:
+		map<int,Project*> mProjects;
+		map<int,Service*> mServices;
+		map<int,Country*> mCountries;
+		map<int,Provider*> mProviders;    
+	};
+	
+}
